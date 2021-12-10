795724e3ed09b6ea4bbe7c1ac773c75363b21098
Projet AP <projet_ap@yanish.fr>
2021-12-10 10:13:22 +0000

    Modification VueReservation

--- view/vueReservation.php
+++ view/vueReservation.php
@@ -1,13 +1,31 @@
 <div class="col-md-12 ml-md-auto mr-md-auto">
-<div class="form-field">
-  <label class="form-field__label">Datepicker Demo</label>
-  <input id="datepicker" type="text">
-</div>
-<script>
-    /* javascript */
-const datePicker = MCDatepicker.create({
-  el: '#datepicker',
-  bodyType: 'modal'
-});
-</script>
+    <div class="form-field">
+        <label class="form-field__label">Datepicker Demo</label>
+        <input id="datepicker" type="text">
+    </div>
+        <?php
+        $Date = new DateTime();
+        $jour = date('d');
+        $mois = date('m');
+        $annee = date('y');
+
+        var_dump($jour);
+        var_dump($mois);
+        var_dump($annee);
+        print(date("m.d.y")); 
+        ?>
+    <script>
+        const datePicker = MCDatepicker.create({
+            el: '#datepicker',
+            bodyType: 'modal',
+            customWeekDays: ['Dimanche','Lundi', 'Mardi', 'Mecredi', 'Jeudi', 'Vendredi', 'Samedi'],
+            customMonths: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+            customClearBTN: 'Annuler',
+            customCancelBTN: 'Retour',
+
+            firstWeekday: 1,
+            minDate: new Date(<?= $annee ?>, <?= $mois ?>, <?= $jour ?>)
+
+        });
+    </script>
 </div>
